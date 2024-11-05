e1f0538b334383553b523adb86696960e7c0da06
clepfeed <clepfeed@yandex.by>
2024-11-05 22:40:38 +0300

    start

---
+++ iterativeЬethods/mian.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include <math.h>
+#include <iomanip>
+#include <chrono>
+#include <fstream>
+
+using namespace std;
+
+int k_max = 1000;
+double eps = 1e-7;
+
+int main()
+{
+	int n = 1000;
+	int m = 12;
+
+
+
+}
