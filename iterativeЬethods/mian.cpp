364e1ce435523d48287a6ee69db8da7cf91a309f
clepfeed <clepfeed@yandex.by>
2024-11-06 10:27:36 +0300

    complete

--- iterativeЬethods/mian.cpp
+++ iterativeЬethods/mian.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 int k_max = 1000;
 double eps = 1e-7;
-double w = 0.5;
+
 
 void printVec(vector<vector<double>>& mat)
 {
@@ -24,6 +24,7 @@ void printVec(vector<vector<double>>& mat)
 		{
 			cout << mat[q][i] << " ";
 		}
+		cout << "\n";
 	}
 }
 void printVec(vector<double>& mat)
@@ -53,16 +54,115 @@ void mulMat(vector<vector<double>>& matA, vector<double>& x, vector<double>& mat
 	}
 }
 
-int Jacobi(vector<vector<double>>& matA, vector<double>& x0, vector<double>& b)
+double norm(vector<double>& x0, vector<double>& x)
 {
-	int g = 0;
-	vector<double> x(0, x0.size());
-	for (int i = 0; i < matA; i++)
+	double normSol = 0;
+	for (int i = 0; i < x0.size(); i++)
 	{
-
+		normSol += abs(x0[i]);
 	}
+	double normSolImprecise = 0;
+	for (int i = 0; i < x0.size(); i++)
+	{
+		normSolImprecise += abs(x0[i] - x[i]);
+	}
+	return normSolImprecise / normSol;
+}
 
-	return g;
+double normAbs(vector<double>& x0, vector<double>& x)
+{
+	double normSolImprecise = 0;
+	for (int i = 0; i < x0.size(); i++)
+	{
+		normSolImprecise += abs(x0[i] - x[i]);
+	}
+	return normSolImprecise;
+}
+
+int Jacobi(vector<vector<double>>& a, vector<double>& x0, vector<double>& b)
+{
+	int k = 1;
+	vector<double> x(0, x0.size());
+	x = x0;
+	for (; k < k_max; k++)
+	{
+		x0 = x; // запоминаем вектор шага k
+		for (int i = 0; i < x0.size(); i++) // все x в векторе решений
+		{
+			x[i] = b[i];
+			double sum = 0;
+			int j = 0;
+			for (; j < a[i].size() && j < i; j++) // первая сумма до i - 1
+			{
+				sum += a[i][j] * x0[j];
+			}
+			for (; j < a.size() && j < i; j++)
+			{
+				sum += a[j][i] * x0[j];
+			}
+			x[i] -= sum;
+			sum = 0;
+			j++; // i + 1
+			for (; j < a[i].size(); j++) // вторая сумма до n
+			{
+				sum += a[i][j] * x0[j];
+			}
+			for (; j < a.size(); j++)
+			{
+				sum += a[j][i] * x0[j];
+			}
+			x[i] -= sum;
+			x[i] /= a[i][i];
+			//cout << x[i] << " ";
+		}
+		if (norm(x0, x) < eps)
+			break;
+	}
+	x0 = x;
+	return k;
+}
+
+int relaxation(vector<vector<double>>& a, vector<double>& x0, vector<double>& b, double w)
+{
+	int k = 1;
+	vector<double> x(0, x0.size());
+	x = x0;
+	for (; k < k_max; k++)
+	{
+		x0 = x; // запоминаем вектор шага k
+		for (int i = 0; i < x0.size(); i++) // все x в векторе решений
+		{
+			x[i] = b[i];
+			double sum = 0;
+			int j = 0;
+			for (; j < a[i].size() && j < i; j++) // первая сумма до i - 1
+			{
+				sum += a[i][j] * x[j];
+			}
+			for (; j < a.size() && j < i; j++)
+			{
+				sum += a[j][i] * x[j];
+			}
+			x[i] -= sum;
+			sum = 0;
+			j++; // i + 1
+			for (; j < a[i].size(); j++) // вторая сумма до n
+			{
+				sum += a[i][j] * x0[j];
+			}
+			for (; j < a.size(); j++)
+			{
+				sum += a[j][i] * x0[j];
+			}
+			x[i] -= sum;
+			x[i] *= (w / a[i][i]);
+			x[i] = (1 - w) * x0[i] + x[i];
+		}
+		if (norm(x0, x) < eps)
+			break;
+	}
+	x0 = x;
+	return k;
 }
 
 int main()
@@ -71,13 +171,12 @@ int main()
 
 	int n = 1000;
 	int m = 12;
+	vector<double> w = {0.5, 1, 1.5};
 
-	vector<vector<double>> matAOriginal;
 	vector<vector<double>> matA;
 
 	vector<double> b(n, 0);
 
-	vector<double> solutionOriginal(n, 0);
 	vector<double> solution(n, 0);
 	vector<double> solutionImprecise(n, 0);
 	vector<double> x0(n, 0);
@@ -105,24 +204,42 @@ int main()
 		}
 		matA[i][i] = rand() % 120 + 12.0 + sum;
 	}
-	matAOriginal = matA;
 	for (double i = 0; i < n; i++)
 	{
 		solution[i] = m + i;
 	}
-	solutionOriginal = solution;
-
-	mulMat(matAOriginal, solutionOriginal, b);
+	mulMat(matA, solution, b);
 
 	cout << "x0:\n";
 	printVec(x0);
-
 	cout << "Solution:\n";
-	printVec(solutionOriginal);
+	printVec(solution);
 	cout << "\n";
 
-	cout << "w = " << w << "\n"; // w = 0.5
-	cout << "q = " << Jacobi(matA, x0, b) << "\n";
-	printVec(x0);
+	cout << std::fixed << std::setprecision(22);
 
+	cout << "Jacobi:\n";
+
+	solutionImprecise = x0;
+	cout << "q = " << Jacobi(matA, solutionImprecise, b) << "\n";
+	cout << "Imprecise solution:\n";
+	printVec(solutionImprecise);
+	cout << "Relative error: " << norm(solution, solutionImprecise) << "\n";
+	cout << "Absolute error: " << normAbs(solution, solutionImprecise) << "\n";
+
+
+	cout << "\nRalaxation method:\n";
+	for (int i = 0; i < w.size(); i++)
+	{
+		cout << std::fixed << std::setprecision(3);
+		cout << "w: " << w[i] << "\n";
+		solutionImprecise = x0;
+		cout << "q = " << relaxation(matA, solutionImprecise, b, w[i]) << "\n";
+		cout << std::fixed << std::setprecision(22);
+		cout << "Imprecise solution:\n";
+		printVec(solutionImprecise);
+		cout << "Relative error: " << norm(solution, solutionImprecise) << "\n";
+		cout << "Absolute error: " << normAbs(solution, solutionImprecise) << "\n\n";
+	}
+	return 0;
 }
